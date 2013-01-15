2d9607cc3a11128e9fc6bac0ea05cd9ff8ba2f30
Mattias Lyckne <mattias.lyckne@gmail.com>
2013-01-15 21:31:23 +0100

    Added Listener and Response objects

---
+++ src/listener.js
@@ -0,0 +1,28 @@
+var Listener = (function() {
+
+	function Listener(robot, regex, callback) {
+		this.robot = robot;
+		this.regex = regex;
+		this.callback = callback;
+	}
+
+	Listener.prototype = {
+
+		call: function(msg) {
+			var matches = msg.match(this.regex);
+			console.log(matches);
+			if(matches) {
+				this.callback( new this.robot.Response(this.robot, msg, matches));
+				return true;
+			} else {
+				return false;
+			}
+		}
+	};
+
+
+	return Listener;
+})();
+
+
+module.exports = Listener;
---
+++ src/response.js
@@ -0,0 +1,25 @@
+var Response = (function() {
+
+	function Response(robot, message, matches) {
+		this.robot = robot;
+		this.message = message;
+		this.match = matches;
+	}
+
+	Response.prototype = {
+
+		send: function() {
+			var msg = 1 <= arguments.length ? arguments : [];
+			this.robot.adapter.send(msg);
+		},
+		http: function(url) {
+			return this.robot.http(url);
+		}
+	};
+
+	return Response;
+
+})();
+
+
+module.exports = Response;
