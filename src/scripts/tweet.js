fe992eb2bc37d0655c0ebb3fdedb462f1ba89185
Mattias Lyckne <mattias.lyckne@gmail.com>
2013-01-16 20:55:35 +0100

    Added tweet script.

---
+++ src/scripts/tweet.js
@@ -0,0 +1,20 @@
+module.exports = function(robot) {
+	robot.respond(/tweet (.*)/i, function(msg) {
+		var search = msg.match[1];
+
+		return msg.http('http://search.twitter.com/search.json').query({
+			q: search
+		}).get()(function(err, res, body) {
+			var tweets, tweet;
+			tweets = JSON.parse(body);
+
+			console.log(tweets);
+			if(tweets.results && tweets.results.length > 0) {
+				tweet = tweets.results[0];
+				return msg.send("@"+tweet.from_user+": "+tweet.text);
+			} else {
+				return msg.send("Didn't find tweets...");
+			}
+		});
+	});
+};
